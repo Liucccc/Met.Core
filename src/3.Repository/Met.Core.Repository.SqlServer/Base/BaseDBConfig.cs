f7961ae1d37147fa0f3f12f3c182ec8068cb587c
Hi <974854469@qq.com>
2019-01-15 16:51:38 +0800

    基础结构

---
+++ src/3.Repository/Met.Core.Repository.SqlServer/Base/BaseDBConfig.cs
@@ -0,0 +1,25 @@
+﻿using Met.Core.Common.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Met.Core.Repository.SqlServer
+{
+    /// <summary>
+    /// 静态配置类
+    /// </summary>
+    public static class BaseDBConfig
+    {
+        /// <summary>
+        /// 数据库连接字符串(私有字段)
+        /// </summary>
+        private static readonly string _connectionString = Appsettings.GetConfig("ConnectionStrings", "SqlServerConnString");
+        /// <summary>
+        /// 数据库连接字符串(公有属性)
+        /// </summary>
+        public static string ConnectionString
+        {
+            get { return _connectionString; }
+        }
+    }
+}
